f57f0a0151cf84428c7dcd6270237059421fa8d6
Kevin French <kfrench@gsinet.net>
2019-06-19 14:40:41 -0400

    Add files via upload

---
+++ FindFiles.ps1
@@ -0,0 +1,3 @@
+﻿$filename = '*vv*.*'#you can use wildcards here for name and for extension 
+$searchinfolder = 'C:\Users\kfrench\Desktop\images*' 
+Get-ChildItem -Path $searchinfolder -Filter $filename -Recurse | %{$_.FullName}
