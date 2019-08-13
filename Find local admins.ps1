4112c43344377d64f6eb0f157160cb8570b258ac
Kevin French <kfrench@gsinet.net>
2019-08-13 10:58:59 -0400

    Add files via upload

---
+++ Find local admins.ps1
@@ -0,0 +1,11 @@
+﻿invoke-command {
+$members = net localgroup administrators |
+where {$_ -AND $_ -notmatch "command completed successfully"} |
+select -skip 4
+New-Object PSObject -Property @{
+Computername = $env:COMPUTERNAME
+Group = "Administrators"
+Members=$members
+}
+} -computer fs1,sp01,ncnad -HideComputerName |
+Select * -ExcludeProperty RunspaceID | Export-CSV c:\data\local_admins.csv -NoTypeInformation 
